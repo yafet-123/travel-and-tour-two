1f609abd11fa7f8d97f5ff49ac80db3d94ab16ae
yafet-123 <yafetaddisu123@gmail.com>
2023-01-12 11:55:09 -0800

    adding css file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'drive.google.com',
+      'www.shadowsofafrica.com',
+      'www.responsibletravel.com',
+      'img.freepik.com',
+      'accounting.binus.ac.id',
+      'rmi.org',
+      'media.istockphoto.com',
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
